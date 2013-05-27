b3f72314cefa4f4fa0e476c9f851e7a843dd13cf
lilongen <lilongen@163.com>
2013-05-27 22:05:50 +0800

    add lines to tt3

--- tt3.pl
+++ tt3.pl
@@ -71,3 +71,18 @@ $b =~ s/#A#/aaa/;
 $b =~ s/#B#/bbb/;
 
 print $a . "   " . $b;
+total 948
+-rwxrwxr-x 1 lilongen lilongen   1040 May 26 02:32 construct.view.data.pl
+-rwxrwxr-x 1 lilongen lilongen   1839 May 26 02:32 cpa.pl
+-rw-rw-r-- 1 lilongen lilongen 901120 May 26 02:32 cpa.tar
+-rwxrwxr-x 1 lilongen lilongen   9222 May 26 02:32 digitalize.methods.usage.pl
+-rwxrwxr-x 1 lilongen lilongen   1056 May 26 02:32 digitalize.self.classes.methods.pl
+-rwxrwxr-x 1 lilongen lilongen   4593 May 26 02:32 generate.report.pl
+-rwxrwxr-x 1 lilongen lilongen   1670 May 26 02:32 list.classes.methods.sh
+-rwxrwxr-x 1 lilongen lilongen    981 May 26 02:32 pre.scan.sh
+-rw-rw-r-- 1 lilongen lilongen     39 May 26 02:32 README.md
+-rwxrwxr-x 1 lilongen lilongen   6418 May 26 02:32 scan.methods.usage.pl
+-rwxrwxr-x 1 lilongen lilongen   1429 May 26 02:32 tt3.pl
+-rwxrwxr-x 1 lilongen lilongen   1429 May 26 02:32 tt.pl
+-rwxrwxr-x 1 lilongen lilongen   2203 May 26 02:32 Util.pm
+drwxrwxr-x 6 lilongen lilongen   4096 May 26 02:32 web
